6f9e64f723222a0e6148edce5bb2645fb355dea4
devhiko <aydin96tr@gmail.com>
2022-04-20 19:41:13 +0300

    come to cloud functions

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -2,14 +2,16 @@ export const createProject = (project) => {
   return (dispatch, getState, { getFirebase, getFirestore }) => {
     // async call to db
     const firestore = getFirestore();
+    const profile = getState().firebase.profile;
+    const authorID = getState().firebase.auth.uid;
     // returns promise and should add then()
     firestore.collection('projects')
       .add({
         // ... -> project.state (title, author)
         ...project,
-        authorFirstName: 'Hikmet',
-        authorLastName: 'Ken',
-        authorID: 12345,
+        authorFirstName: profile.firstName,
+        authorLastName: profile.lastName,
+        authorID: authorID,
         createdAt: new Date()
       })
       .then(() => { dispatch({ type: 'CREATE_PROJECT', project }) })
