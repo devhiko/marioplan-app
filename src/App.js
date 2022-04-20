cc9cd299f576358a66dc152a33e73aad637ad222
devhiko <aydin96tr@gmail.com>
2022-04-20 23:09:16 +0300

    project ready for build

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Dashboard from './components/dashboard/Dashboard'
 import ProjectDetails from './components/projects/ProjectDetails'
@@ -10,7 +10,7 @@ import CreateProject from './components/projects/CreateProject'
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
+      <Router>
         <div className="App">
           <Navbar />
           <Switch>
@@ -21,7 +21,7 @@ class App extends Component {
             <Route path='/create' component={CreateProject} />
           </Switch>
         </div>
-      </BrowserRouter>
+      </Router>
     );
   }
 }
